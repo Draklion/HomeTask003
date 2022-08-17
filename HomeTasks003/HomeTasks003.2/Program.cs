0f58e9415c18b34386e3d30e0cc8e007c98bbddf
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-17 08:30:41 +0500

    HomeTask003.2 completed.

--- HomeTasks003/HomeTasks003.2/Program.cs
+++ HomeTasks003/HomeTasks003.2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+  Console.WriteLine("Задание 2. Программа, которая по заданому номеру четверти, показывает диапазон возможных координат точек в этой четверти.");
+
+  Console.Write("Введите номер четверти: ");
+  int quarterNumber = Int32.Parse(Console.ReadLine());
+  switch (quarterNumber)
+  {
+    case 1:
+      Console.WriteLine("Диапазон первой четверти по X и Y от 1 до N");
+      break;
+    case 2:
+      Console.WriteLine("Диапазон второй четверти по X от -1 до -N, по Y от 1 до N");
+      break;
+    case 3:
+      Console.WriteLine("Диапазон третье четверти по X от -1 до -N, по Y от -1 до -N");
+      break;
+    case 4:
+      Console.WriteLine("Диапазон четвертой четверти по X  от 1 до N, по Y от -1 до -N");
+      break;
+  }
+}
+
+catch
+{
+  Console.WriteLine("Вы ввели некорректное значение.");
+}

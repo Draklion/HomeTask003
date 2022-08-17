1e15efe530426381ee10703edd5a3419e942a0f8
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-17 09:33:16 +0500

    HomeTask003.5 completed.

--- HomeTasks003/HomeTasks003.5/Program.cs
+++ HomeTasks003/HomeTasks003.5/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+  Console.WriteLine("Задание 5. Программа, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N.");
+
+  Console.WriteLine("Введите число от 1 до N: ");
+  int number = Int32.Parse(Console.ReadLine());
+
+  if (number < 1)
+    Console.WriteLine("Вы ввели значение меньше 1.");
+  else
+  {
+    Console.Write($"Таблица кубов от 1 до {number}: ");
+    for (int i = 1; i <= number; i++)
+    {
+      Console.Write(Math.Pow(i, 3));
+      if (i != number)
+        Console.Write(", ");
+    }
+  }
+}
+catch
+{
+  Console.WriteLine("Вы ввели некорректное значение.");
+}

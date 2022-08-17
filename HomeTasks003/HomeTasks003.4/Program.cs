e41da510bc2e4a1201e2de71d07a59baabee3be9
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-17 09:26:07 +0500

    Added changes.

--- HomeTasks003/HomeTasks003.4/Program.cs
+++ HomeTasks003/HomeTasks003.4/Program.cs
@@ -2,14 +2,20 @@
 {
   Console.WriteLine("Задание 4. Программа, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N.");
 
-  Console.WriteLine("Введите число: ");
+  Console.WriteLine("Введите число от 1 до N: ");
   int number = Int32.Parse(Console.ReadLine());
-  Console.Write($"Таблица квадратовот 1 до {number}: ");
-  for (int i = 1; i <= number; i++)
+
+  if (number < 1)
+    Console.WriteLine("Вы ввели значение меньше 1.");
+  else
   {
-    Console.Write(Math.Pow(i, 2));
-    if (i != number)
-      Console.Write(", ");
+    Console.Write($"Таблица квадратов от 1 до {number}: ");
+    for (int i = 1; i <= number; i++)
+    {
+      Console.Write(Math.Pow(i, 2));
+      if (i != number)
+        Console.Write(", ");
+    }
   }
 }
 catch

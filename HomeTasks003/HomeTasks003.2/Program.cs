0c6d257e673179addb88e9775f66567c2fd9b720
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-17 08:37:14 +0500

    Added changes.

--- HomeTasks003/HomeTasks003.2/Program.cs
+++ HomeTasks003/HomeTasks003.2/Program.cs
@@ -4,21 +4,26 @@
 
   Console.Write("Введите номер четверти: ");
   int quarterNumber = Int32.Parse(Console.ReadLine());
-  switch (quarterNumber)
+  if (quarterNumber > 0 && quarterNumber <= 4)
   {
-    case 1:
-      Console.WriteLine("Диапазон первой четверти по X и Y от 1 до N");
-      break;
-    case 2:
-      Console.WriteLine("Диапазон второй четверти по X от -1 до -N, по Y от 1 до N");
-      break;
-    case 3:
-      Console.WriteLine("Диапазон третье четверти по X от -1 до -N, по Y от -1 до -N");
-      break;
-    case 4:
-      Console.WriteLine("Диапазон четвертой четверти по X  от 1 до N, по Y от -1 до -N");
-      break;
+    switch (quarterNumber)
+    {
+      case 1:
+        Console.WriteLine("Диапазон первой четверти по X и Y от 1 до N");
+        break;
+      case 2:
+        Console.WriteLine("Диапазон второй четверти по X от -1 до -N, по Y от 1 до N");
+        break;
+      case 3:
+        Console.WriteLine("Диапазон третье четверти по X от -1 до -N, по Y от -1 до -N");
+        break;
+      case 4:
+        Console.WriteLine("Диапазон четвертой четверти по X  от 1 до N, по Y от -1 до -N");
+        break;
+    }
   }
+  else
+    Console.WriteLine("Вы ввели неверный номер четверти.");
 }
 
 catch
